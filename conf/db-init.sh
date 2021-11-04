0851bddd50f21e4d1e763fa415262d0815f32a35
eirblaze <eirblaze@live.jp>
2021-11-04 14:52:34 +0900

    インデント更新

--- conf/db-init.sh
+++ conf/db-init.sh
@@ -13,55 +13,55 @@ if (mysqladmin -u root  -pwordpress ping -h localhost --silent) then
   return 0
 fi
 
-  # /usr/bin/mysql_install_db --user=root > /dev/null
+echo "[i] MySQL data directory not found, creating initial DBs"
 
-  if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
-    MYSQL_ROOT_PASSWORD=111111
-    echo "[i] MySQL root Password: $MYSQL_ROOT_PASSWORD"
-  fi
+# /usr/bin/mysql_install_db --user=root > /dev/null
 
-  MYSQL_DATABASE=${MYSQL_DATABASE:-""}
-  MYSQL_USER=${MYSQL_USER:-""}
-  MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
+if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
+  MYSQL_ROOT_PASSWORD=111111
+  echo "[i] MySQL root Password: $MYSQL_ROOT_PASSWORD"
+fi
 
-  tfile=`mktemp`
-  if [ ! -f "$tfile" ]; then
-    echo "[x] mktemp error."
-    return 1
-  fi
+MYSQL_DATABASE=${MYSQL_DATABASE:-""}
+MYSQL_USER=${MYSQL_USER:-""}
+MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
 
-  cat << EOF > $tfile
+tfile=`mktemp`
+if [ ! -f "$tfile" ]; then
+  echo "[x] mktemp error."
+  return 1
+fi
+
+cat << EOF > $tfile
 USE mysql;
 FLUSH PRIVILEGES;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD" WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
 EOF
 
-  if [ "$MYSQL_DATABASE" != "" ]; then
-    MYSQL_CHARACTER_SET=${MYSQL_CHARACTER_SET:+"CHARACTER SET $MYSQL_CHARACTER_SET"}
-    MYSQL_COLLATE=${MYSQL_COLLATE:+"COLLATE $MYSQL_COLLATE"}
+if [ "$MYSQL_DATABASE" != "" ]; then
+  MYSQL_CHARACTER_SET=${MYSQL_CHARACTER_SET:+"CHARACTER SET $MYSQL_CHARACTER_SET"}
+  MYSQL_COLLATE=${MYSQL_COLLATE:+"COLLATE $MYSQL_COLLATE"}
 
-    echo "[i] Creating database: $MYSQL_DATABASE"
-    echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` $MYSQL_CHARACTER_SET $MYSQL_COLLATE;" >> $tfile
+  echo "[i] Creating database: $MYSQL_DATABASE"
+  echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` $MYSQL_CHARACTER_SET $MYSQL_COLLATE;" >> $tfile
 
-    if [ "$MYSQL_USER" != "" ]; then
-      echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
-      echo "alter user '$MYSQL_USER'@'%' identified by '$MYSQL_PASSWORD';" >> $tfile
-      echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
-    fi
+  if [ "$MYSQL_USER" != "" ]; then
+    echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
+    echo "alter user '$MYSQL_USER'@'%' identified by '$MYSQL_PASSWORD';" >> $tfile
+    echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
   fi
+fi
 
-  echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> $tfile
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> $tfile
 
-  cat $tfile
-  /usr/bin/mysqld --user=root --bootstrap --verbose=1 < $tfile
-  # mysql –u username –p new_db_name < dump_file.sql
-  # mysql --user=root < $tfile
-  rm -f $tfile
+cat $tfile
+/usr/bin/mysqld --user=root --bootstrap --verbose=1 < $tfile
+# mysql –u username –p new_db_name < dump_file.sql
+# mysql --user=root < $tfile
+rm -f $tfile
 
-  # mysqladmin password "$MYSQL_ROOT_PASSWORD"
-# fi
-
+# mysqladmin password "$MYSQL_ROOT_PASSWORD"
 
 # exec /usr/bin/mysqld --user=root --console
 
